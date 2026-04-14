0db1198d5267fbdcc6a65c6a50d0d481a3645e62
manuader <adermanu@gmail.com>
2026-04-13 23:33:03 -0300

    feat: register process-batch in Inngest route

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,8 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { executeResearch } from "@/lib/inngest/functions/execute-research";
+import { processBatch } from "@/lib/inngest/functions/process-batch";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [executeResearch],
+  functions: [executeResearch, processBatch],
 });
